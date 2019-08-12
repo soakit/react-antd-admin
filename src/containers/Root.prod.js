1c5549dd010d578d0db378f9c40e0fc9d2cf7aa1
soakit <liuchang2024@gmail.com>
2019-08-12 10:12:49 +0800

    update

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import route from '../route';
-import { HashRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { HashRouter as Router } from 'react-router-dom'
+import route from '../route'
 
 export default class Root extends Component {
   render() {
-    const { store } = this.props;
-    if (!this.route) this.route = route;
+    const { store } = this.props
+    if (!this.route) this.route = route
     return (
       <Provider store={store}>
-        <Router children={this.route}/>
+        <Router>{this.route}</Router>
       </Provider>
-    );
+    )
   }
 }
